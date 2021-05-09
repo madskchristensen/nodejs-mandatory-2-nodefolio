1660eb47f3e0091df6fd21f7d2d4e4a6d5db2c23
Mads <maller2650@gmail.com>
2021-05-09 22:34:45 +0200

    updated port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
 
 const express = require("express")
 const app = express()
-const port = process.env.port;
+let port = process.env.port || 8080;
 const fs = require("fs")
 
 app.use(express.static("public"))
